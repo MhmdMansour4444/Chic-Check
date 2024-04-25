0ec3acbbf3924ea5d5ef5138d99711479bf9b26d
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-04-26 00:28:45 +0300

    some migrations added

---
+++ backend/database/migrations/2024_04_25_212344_create_clothes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+
+        Schema::create('clothes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 100);
+            $table->string('type', 50);
+            $table->string('brand', 50)->nullable();
+            $table->string('size', 20)->nullable();
+            $table->string('color', 50)->nullable();
+            $table->string('material', 50)->nullable();
+            $table->string('image_path', 255);
+            $table->unsignedBigInteger('category_id');
+            $table->tinyInteger('is_public')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('category_id')->references('category_id')->on('categories');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('clothes');
+    }
+};
