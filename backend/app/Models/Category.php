758bfe5b51388e4e07e3cb433352c8472c37ea9a
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-04-26 01:15:25 +0300

    category model

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
 class Category extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function clothes()
+    {
+        return $this->hasMany(Cloth::class, 'category_id');
+    }
+
 }
