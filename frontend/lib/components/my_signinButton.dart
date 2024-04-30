a9442ce338f40d96f38cc3c48c647b22e03bc8b8
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-04-30 04:42:23 +0300

    feat(button): sign in button component added

---
+++ frontend/lib/components/my_signinButton.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MySignInButton extends StatelessWidget {
+  const MySignInButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Sign In'),
+      ),
+    );
+  }
+}
