98ffa492f5696f306c489b684a524af201699434
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-02 05:18:14 +0300

    feat(button): fixed the go to scan button

--- frontend/lib/pages/random_page.dart
+++ frontend/lib/pages/random_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class RandomHomePage extends StatelessWidget {
+  void onTap() {
+    // Implement onTap functionality here
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,19 +13,33 @@ class RandomHomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 16.0),
+        padding: const EdgeInsets.only(left: 30.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 100),
             const Text(
               'Use your camera for our fitcheck',
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Click Me'),
+            GestureDetector(
+              onTap: onTap,
+              child: Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFF6678),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  'Go to Scan',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
