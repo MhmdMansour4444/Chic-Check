897dadb7cd5ab5f961e812e59e275fbd1edacbb3
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-02 06:22:52 +0300

    feat(navbar): added nav bar icons

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,85 +16,96 @@ class _HomePageState extends State<HomePage> {
         title: Text('Welcome Mohammad!'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 30),
-          Container(
-            padding: EdgeInsets.all(40),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(108, 255, 102, 120),
-              borderRadius: BorderRadius.circular(10),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.all(40),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(108, 255, 102, 120),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Column(
+                children: [
+                  Text(
+                    'Need help with your outfit?',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'We are here to assist you!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Column(
-              children: [
-                Text(
-                  'Need help with your outfit?',
+            SizedBox(height: 20),
+            const Text(
+              'Use your camera for our fitcheck',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: onTap,
+              child: Container(
+                padding: const EdgeInsets.only(
+                    bottom: 12, left: 25, right: 25, top: 12),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFF6678),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const Text(
+                  'Go to Scan',
                   style: TextStyle(
-                    fontSize: 24,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'We are here to assist you!',
+              ),
+            ),
+            SizedBox(height: 30),
+            const Text(
+              'Or browse our community for the best outfit advices out there!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: onTap,
+              child: Container(
+                padding: const EdgeInsets.only(
+                    bottom: 12, left: 25, right: 25, top: 12),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFF6678),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const Text(
+                  'Start Chatting',
                   style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
                 ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Use your camera for our fitcheck',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-          ),
-          SizedBox(height: 20),
-          GestureDetector(
-            onTap: onTap,
-            child: Container(
-              padding: const EdgeInsets.only(
-                  bottom: 12, left: 25, right: 25, top: 12),
-              decoration: BoxDecoration(
-                color: const Color(0xFFFF6678),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: const Text(
-                'Go to Scan',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
               ),
             ),
-          ),
-          SizedBox(height: 30),
-          Text(
-            'Or browse our community for the best outfit advices out there!',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
-          ),
-          SizedBox(height: 20),
-          GestureDetector(
-            onTap: onTap,
-            child: Container(
-              padding: const EdgeInsets.only(
-                  bottom: 12, left: 25, right: 25, top: 12),
-              decoration: BoxDecoration(
-                color: const Color(0xFFFF6678),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: const Text(
-                'Start Chatting',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: GNav(
+        tabs: [
+          GButton(icon: Icons.home),
+          GButton(icon: Icons.checkroom_rounded),
+          GButton(icon: Icons.camera_outlined),
+          GButton(icon: Icons.message_rounded),
+          GButton(icon: Icons.bookmarks_outlined),
         ],
       ),
     );
