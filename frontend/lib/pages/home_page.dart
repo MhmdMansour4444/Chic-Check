6d51a9d5c2de0091824bb4085ce1a25b842ec803
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-02 05:46:20 +0300

    feat(homepage): instructions and styling fixed

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  void onTap() {
-    // Implement onTap functionality here
-  }
+  void onTap() {}
 
   @override
   Widget build(BuildContext context) {
@@ -12,40 +10,74 @@ class HomePage extends StatelessWidget {
         title: Text('Welcome Mohammad!'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 2.0),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 100),
-              const Text(
-                'Use your camera for our fitcheck',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+      body: Column(
+        children: [
+          SizedBox(height: 30),
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Need help with your outfit?',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: onTap,
-                child: Container(
-                  padding: const EdgeInsets.only(
-                      bottom: 12, left: 25, right: 25, top: 12),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFF6678),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: const Text(
-                    'Go to Scan',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Use your camera for our fitcheck',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+          ),
+          SizedBox(height: 20),
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              padding: const EdgeInsets.only(
+                  bottom: 12, left: 25, right: 25, top: 12),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFF6678),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Text(
+                'Go to Scan',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+          SizedBox(height: 30),
+          Text(
+            'Or browse our community for the best outfit advices out there!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+          ),
+          SizedBox(height: 20),
+          GestureDetector(
+            onTap: onTap,
+            child: Container(
+              padding: const EdgeInsets.only(
+                  bottom: 12, left: 25, right: 25, top: 12),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFF6678),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Text(
+                'Start Chatting',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
