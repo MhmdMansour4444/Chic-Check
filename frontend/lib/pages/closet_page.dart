e59bce949d3c1634ac4bc6ec52d14ddcf6007fcf
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-02 06:53:20 +0300

    feat(navbar): added pages with respect to the icons of the navbar to navigate to 3

---
+++ frontend/lib/pages/closet_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ClosetPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Closet Page',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/pages/forums_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ForumsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Forums Page',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
