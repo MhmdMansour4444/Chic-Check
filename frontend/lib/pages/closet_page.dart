19ecc7cf0d58d763aaa6de06adff6808154d634d
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-03 13:29:37 +0300

    feat(pages): added navbar to closet page and simplified the gridview with gridview builder

--- frontend/lib/pages/closet_page.dart
+++ frontend/lib/pages/closet_page.dart
@@ -1,54 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/home_page.dart';
-import 'package:frontend/pages/closet_page.dart';
-import 'package:frontend/pages/camera_page.dart';
-import 'package:frontend/pages/forums_page.dart';
 import 'package:frontend/components/my_navbar.dart';
 
-class ClosetPage extends StatelessWidget {
+class ClosetPage extends StatefulWidget {
+  @override
+  _ClosetPageState createState() => _ClosetPageState();
+}
+
+class _ClosetPageState extends State<ClosetPage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
       ),
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: [
-          Padding(
+      body: GridView.builder(
+        itemCount: 5,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (context, index) {
+          return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
               height: 30,
               width: 30,
               color: Colors.grey[300],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 30,
-              width: 30,
-              color: Colors.grey[300],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 30,
-              width: 30,
-              color: Colors.grey[300],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 30,
-              width: 30,
-              color: Colors.grey[300],
-            ),
-          ),
-        ],
+          );
+        },
+      ),
+      bottomNavigationBar: MyNavBar(
+        selectedIndex: _selectedIndex,
+        onTabChange: _navigateBottomBar,
       ),
     );
   }
 }
+
+// GridView.count(
+      //   crossAxisCount: 3,
+      //   children: [
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Container(
+      //         height: 30,
+      //         width: 30,
+      //         color: Colors.grey[300],
+      //       ),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Container(
+      //         height: 30,
+      //         width: 30,
+      //         color: Colors.grey[300],
+      //       ),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Container(
+      //         height: 30,
+      //         width: 30,
+      //         color: Colors.grey[300],
+      //       ),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Container(
+      //         height: 30,
+      //         width: 30,
+      //         color: Colors.grey[300],
+      //       ),
+      //     ),
+      //   ],
+      // ),
