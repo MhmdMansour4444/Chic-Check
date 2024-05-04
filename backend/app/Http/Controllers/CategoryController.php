eac76d0515556e53f8e298aebf365e4fa61ca130
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-04 11:20:06 +0300

    feat(controllers): added category controller

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:50',
+        ]);
+
+        $category = new Category([
+            'name' => $request->name,
+        ]);
+
+        $category->save();
+
+        return response()->json(['message' => 'Category created successfully'], 201);
+    }
+
+    public function read(Request $request)
+    {
+        $categories = Category::all();
+
+        return response()->json($categories, 200);
+    }
+
+    public function update(Request $request, $categoryId)
+    {
+        $request->validate([
+            'name' => 'required|string|max:50',
+        ]);
+
+        $category = Category::findOrFail($categoryId);
+
+        $category->name = $request->name;
+        $category->save();
+
+        return response()->json(['message' => 'Category updated successfully'], 200);
+    }
+
+    public function delete($categoryId)
+    {
+        $category = Category::findOrFail($categoryId);
+        $category->delete();
+
+        return response()->json(['message' => 'Category deleted successfully'], 200);
+    }
 }
