79bda48fd5a8a08045c84ad62f14e5a277a73179
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-04 09:58:39 +0300

    feat(pages): added content to savedPage

--- frontend/lib/pages/saved_page.dart
+++ frontend/lib/pages/saved_page.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/components/my_navbar.dart';
 
+class SavedPage extends StatefulWidget {
+  const SavedPage({super.key});
+
+  @override
+  State<SavedPage> createState() => _SavedPageState();
+}
+
+class _SavedPageState extends State<SavedPage> {
+  int _selectedIndex = 0;
+  int _selectedTitleIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: InkWell(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Color(0xFFFF6678),
+                  width: 3.0,
+                ),
+              ),
+            ),
+            child: const Text(
+              'The Closet',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: MyNavBar(
+        selectedIndex: _selectedIndex,
+        onTabChange: _navigateBottomBar,
+      ),
+    );
+  }
+}
