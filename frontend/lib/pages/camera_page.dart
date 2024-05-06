b820d91c13cba10cb5a0479ff2ffe59ea935688f
mohammad mansour <mhmd.mans.2001@gmail.com>
2024-05-06 10:34:49 +0300

    frontend: feat(camera): added camera page with some functionalities

--- frontend/lib/pages/camera_page.dart
+++ frontend/lib/pages/camera_page.dart
@@ -1,13 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:io';
 
-class CameraPage extends StatelessWidget {
+class CameraPage extends StatefulWidget {
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController _controller;
+  bool _isCameraReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    final backCamera = cameras.last;
+    _controller = CameraController(
+      backCamera,
+      ResolutionPreset.medium,
+    );
+
+    _controller.initialize().then((_) {
+      if (!mounted) return;
+      setState(() {
+        _isCameraReady = true;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Camera Page',
-        style: TextStyle(fontSize: 50),
+    if (!_isCameraReady) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Camera Page'),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera Page'),
+        centerTitle: true,
       ),
+      body: CameraPreview(_controller),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera),
+        onPressed: () async {
+          try {
+            final image = await _controller.takePicture();
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPicturePage(imagePath: image.path),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DisplayPicturePage extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPicturePage({Key? key, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display Picture')),
+      body: Image.file(File(imagePath)),
     );
   }
 }
